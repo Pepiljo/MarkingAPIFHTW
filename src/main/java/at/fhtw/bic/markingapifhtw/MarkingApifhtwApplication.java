7e910d792465a2c36f10ed7ce72f4fbd3d705425
Pepiljo <knezev13@gmail.com>
2022-12-21 18:06:23 +0100

    Initial commit

---
+++ src/main/java/at/fhtw/bic/markingapifhtw/MarkingApifhtwApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic.markingapifhtw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkingApifhtwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkingApifhtwApplication.class, args);
+	}
+
+}
